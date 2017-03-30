edfdcc03769b2b0bf0e890c109ff6a273db6b498
Chloé Gence <chloe.gence@gmail.com>
2017-03-30 03:54:19 +0200

    fix nav links

--- README.md
+++ README.md
@@ -4,4 +4,19 @@ git clone https://github.com/Insoumis/opif.git
 npm install
 npm start
 ```
-Ouvrir votre navigateur à l'adresse `http://localhost:8000/``
+Ouvrir votre navigateur à l'adresse `http://localhost:8000/`
+
+## Autres commandes
+```js
+// Lancer le serveur
+npm start
+// Build le site
+npm run build
+// Tester le build sur un serveur local
+npm run build:serve
+// Build et déployer le site en "production"
+npm run deploy
+```
+
+## Aujouter du contenu
+Pour ajouter du contenu il faut ajouter vos fichiers dans `pages`. Pour l'instant seul le format `Markdown` est pris en compte mais n'hésitez pas à demander si vous avez besoin d'autre chose !
--- templates/Header.js
+++ templates/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { Container } from 'react-grid-system';
+import { prefixLink } from 'gatsby-helpers';
 
 import './Header.scss';
 
@@ -9,8 +10,8 @@ const Header = () => (
     <Container>
       <h1>Opif</h1>
       <nav>
-        <Link activeClassName="active" onlyActiveOnIndex={true} to="/">Accueil</Link>
-        <Link activeClassName="active" to="/methodologie/">La méthodologie</Link>
+        <Link activeClassName="active" onlyActiveOnIndex={true} to={prefixLink('/')}>Accueil</Link>
+        <Link activeClassName="active" to={prefixLink('/methodologie/')}>La méthodologie</Link>
       </nav>
       <div className="baseline"><span>O</span>bservations et <span>P</span>révisions <span>I</span>ncroyablement <span>F</span>iables. Institut de Météorologie Sondagière et d'Horoscope politique.</div>
     </Container>
